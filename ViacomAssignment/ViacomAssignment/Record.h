044ccd64ee77f013a6f9d3fc4b18d57c3f51dd9a
nyprince <risingsun66@gmail.com>
2015-02-23 16:29:56 -0500

    added Record class to hold and parse json info

---
+++ ViacomAssignment/ViacomAssignment/Record.h
@@ -0,0 +1,19 @@
+//
+//  Record.h
+//  ViacomAssignment
+//
+//  Created by Rick Williams on 2/23/15.
+//  Copyright (c) 2015 Rick Williams. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Record : NSObject
+
+@property(strong, nonatomic)NSString *username;
+
+@property(strong, nonatomic)NSString *thumbNail;
+
+@property(strong, nonatomic)NSString *videoLowURL;
+
+@end
---
+++ ViacomAssignment/ViacomAssignment/Record.m
@@ -0,0 +1,13 @@
+//
+//  Record.m
+//  ViacomAssignment
+//
+//  Created by Rick Williams on 2/23/15.
+//  Copyright (c) 2015 Rick Williams. All rights reserved.
+//
+
+#import "Record.h"
+
+@implementation Record
+
+@end
